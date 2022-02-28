83ee7d5f6fa1bfa5c94d2573df2a9ddf9190a85e
SandyMoresoda <sandywyper@gmail.com>
2022-02-28 19:01:38 +0000

    404 page robot

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,23 @@
 import * as React from "react"
 import { GatsbySeo } from "gatsby-plugin-next-seo"
 import Layout from "../components/layout"
+import Robot404 from "../components/Robot404"
 
 const NotFoundPage = () => (
   <>
     <GatsbySeo noindex={true} />
     <Layout>
-      <h1>404: Not Found</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      <section className="page-wrapper">
+        <div className={`md:container pt-32`}>
+          <div className={`bg-cream p-4 md-p-32 h-full pb-32 md:flex`}>
+            <div>
+              <h1>Page Not Found</h1>
+              <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+            </div>
+            <Robot404 classes="max-w-lg" />
+          </div>
+        </div>
+      </section>
     </Layout>
   </>
 )

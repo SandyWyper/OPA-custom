2f993c3cc7778cb8bb20a9365a722230cedec35a
SandyMoresoda <sandywyper@gmail.com>
2022-06-15 15:50:07 +0100

    JSONld for blog/projects page

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,5 +1,5 @@
 import * as React from "react"
-import { GatsbySeo } from "gatsby-plugin-next-seo"
+import { GatsbySeo, BlogJsonLd } from "gatsby-plugin-next-seo"
 import Layout from "../components/layout"
 import PageHero from "../components/pageHero"
 import ProjectCard from "../components/projectCard"
@@ -28,6 +28,13 @@ const Projects = ({
       },
     },
   }
+  const schemaPosts = []
+  allContentfulProject.forEach(project => {
+    schemaPosts.push({
+      headline: project.title,
+      image: `https:${project.image.gatsbyImageData.images.fallback.src}`,
+    })
+  })
 
   return (
     <>
@@ -36,13 +43,21 @@ const Projects = ({
         openGraph={{
           images: [
             {
-              url: headerImage.resize.src,
+              url: `https:${headerImage.resize.src}`,
               width: headerImage.resize.width,
               height: headerImage.resize.height,
               alt: headerImage.title,
             },
           ],
         }}
+      />
+      <BlogJsonLd
+        url="https://oneplanetassociates.com/projects"
+        headline="One Planet Associates past projects"
+        images={`https:${headerImage.resize.src}`}
+        posts={schemaPosts}
+        authorName="One Planet Associates"
+        description="One planet Associates example projects. A variety of case studies looking into the different planning and renewables procurement services. OPA can manage the whole planning process, from early-stage site conception and preparation of pre-planning documents through to securing decisions and discharging planning conditions. OPA pride itself on managing stakeholders effectively and ensuring a transparent service from start to finish."
       />
       <Layout>
         <section className="py-32 page-wrapper">
